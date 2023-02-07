0c56551d562c1bfe247e4b341185267b483f53c9
Kevin Vertongen <kevin.vertongen@gmail.com>
2023-02-07 15:30:04 +0100

    Extend GildedTrosTest with items from TexttestFixture

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -18,4 +18,92 @@ class GildedTrosTest {
         assertEquals(0, app.items[0].quality);
     }
 
+    @Test
+    void givenDegradingItem_WithPositiveSellIn_WhenUpgradeQuality_ThenQualityDecreases() {
+        Item[] items = new Item[]{new Item("Ring of Cleansening Code", 10, 20)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
+
+    @Test
+    void givenImprovingItem_WithPositiveSellIn_WhenUpgradeQuality_ThenQualityIncreases() {
+        Item[] items = new Item[]{new Item("Good Wine", 2, 0)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    void givenLegendaryItem_WithSellInZero_WhenUpgradeQuality_ThenSellInUnchanged_AndQualityUnchanged() {
+        Item[] items = new Item[]{new Item("B-DAWG Keychain", 0, 80)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void givenLegendaryItem_WithNegativeSellIn_WhenUpgradeQuality_ThenSellInUnchanged_AndQualityUnchanged() {
+        Item[] items = new Item[]{new Item("B-DAWG Keychain", -1, 80)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void givenBackStagePass_WithSellInOver10Days_WhenUpgradeQuality_ThenQualityIncreasesByOne() {
+        Item[] items = new Item[]{new Item("Backstage passes for Re:Factor", 15, 20)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    void givenBackStagePass_WithSellIn10Days_WhenUpgradeQuality_ThenQualityIncreasesByTwo() {
+        Item[] items = new Item[]{new Item("Backstage passes for Re:Factor", 10, 40)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(42, app.items[0].quality);
+    }
+
+    @Test
+    void givenBackStagePass_WithSellIn5Days_WhenUpgradeQuality_ThenQualityIncreasesByThree() {
+        Item[] items = new Item[]{new Item("Backstage passes for HAXX", 5, 45)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(48, app.items[0].quality);
+    }
+
+    @Test
+    void givenBackStagePass_WithQuality49_WhenUpgradeQuality_ThenQualityNotOver50() {
+        Item[] items = new Item[]{new Item("Backstage passes for Re:Factor", 10, 49)};
+
+        GildedTros app = new GildedTros(items);
+
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
 }
