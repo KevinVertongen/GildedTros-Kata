2c36733d0e877330f48d0beb74ee60dd5c51d495
Kevin Vertongen <kevin.vertongen@gmail.com>
2023-02-08 09:44:25 +0100

    Refactor updateQuality method

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -1,5 +1,10 @@
 package com.gildedtros;
 
+import com.gildedtros.itemquality.ItemQualityFactory;
+import com.gildedtros.itemquality.ItemQualityStrategy;
+
+import static com.gildedtros.configuration.InventoryConfig.isLegendaryItem;
+
 class GildedTros {
     Item[] items;
 
@@ -8,57 +13,19 @@ class GildedTros {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Good Wine")
-                    && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("B-DAWG Keychain")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (final Item item : items) {
+            final String itemName = item.name;
 
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (isLegendaryItem(itemName)) {
+                continue;
             }
 
-            if (!items[i].name.equals("B-DAWG Keychain")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            final ItemQualityStrategy itemQuality = ItemQualityFactory.getItemQualityStrategy(itemName);
+            final int currentQuality = item.quality;
+            final int currentSellIn = item.sellIn;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Good Wine")) {
-                    if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("B-DAWG Keychain")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            item.quality = itemQuality.calculateQuality(currentQuality, currentSellIn);
+            item.sellIn = currentSellIn - 1;
         }
     }
 }
